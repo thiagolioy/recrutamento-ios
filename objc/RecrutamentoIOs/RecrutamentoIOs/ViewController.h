67c6e44f59bc5889367e0d26be72c0911b2baa77
Thiago Lioy <thiagolioy@Lioy-MacBookPro.local>
2016-01-09 15:16:29 -0200

    initial commit , creating the project , added cocoapods support and related pods

---
+++ objc/RecrutamentoIOs/RecrutamentoIOs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecrutamentoIOs
+//
+//  Created by Thiago Lioy on 1/9/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ objc/RecrutamentoIOs/RecrutamentoIOs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RecrutamentoIOs
+//
+//  Created by Thiago Lioy on 1/9/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
