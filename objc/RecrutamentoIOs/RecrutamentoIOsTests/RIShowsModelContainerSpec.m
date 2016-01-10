56e90f9b2836504dea6eae918eac56c482f72e3f
Thiago Lioy <thiagolioy@Lioy-MacBookPro.local>
2016-01-10 00:58:07 -0200

    added spec to RIShowsModelContainer

---
+++ objc/RecrutamentoIOs/RecrutamentoIOsTests/RIShowsModelContainerSpec.m
@@ -0,0 +1,32 @@
+//
+//  RIShowsModelContainerSpec.m
+//  RecrutamentoIOs
+//
+//  Created by Thiago Lioy on 1/10/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+#import <Specta.h>
+#import <Expecta/Expecta.h>
+#import <TLJsonFactory.h>
+#import "RIShowsModelContainer.h"
+
+SpecBegin(RIShowsModelContainerSpec)
+
+describe(@"RIShowsModelContainer", ^{
+    __block RIShowsModelContainer *modelContainer;
+    beforeAll(^{
+        modelContainer = [RIShowsModelContainer new];
+        modelContainer.shows = @[@1];
+    });
+    it(@"should be 1 obj", ^{
+        expect(modelContainer.shows.count).to.equal(1);
+    });
+    
+    it(@"should be able to add more shows", ^{
+        [modelContainer addMoreShows:@[@2,@3]];
+        expect(modelContainer.shows.count).to.equal(3);
+    });
+});
+
+SpecEnd
